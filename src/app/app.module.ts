595e15231d2387f3a52a6ff288813a1d597ee94d
JoaoVictorDev23 <joaovictor500@gmail.com>
2024-02-01 13:18:03 -0300

    Teste de Evolução - Templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NbThemeModule } from '@nebular/theme';
+import { NbSidebarModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NbThemeModule.forRoot(), NbSidebarModule.forRoot(), NbLayoutModule, NbButtonModule, BrowserAnimationsModule, NbThemeModule.forRoot({ name: 'dark' }), NbEvaIconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
